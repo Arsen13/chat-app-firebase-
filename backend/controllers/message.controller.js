51e7c23ac4edf1d85510151ea42dd607c8da80f3
Kohut Arsen <kohut.arsen@gmail.com>
2024-11-29 20:44:52 +0200

    Create 'deleteMessage' controller

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,3 +1,4 @@
+const { FieldValue } = require("firebase-admin/firestore");
 const { db } = require("../db/firebase");
 const firebase = require("firebase-admin");
 
@@ -20,7 +21,7 @@ const getMessages = async (req, res) => {
         }
 
         res.status(200).json(conversationRef.data().messages);
-        
+
     } catch (error) {
         console.log("Error in getMessages controller", error.message);
         res.status(500).json({ error: "Internal server error" });
@@ -76,4 +77,47 @@ const sendMessage = async (req, res) => {
     }
 }
 
-module.exports = { getMessages, sendMessage };
+const deleteMessage = async (req, res) => {
+    try {
+        const senderId = req.user.id;
+        const { id: messageId } = req.params;
+        
+        const messageDoc = db.collection("messages").doc(messageId);
+        const messageRef = await messageDoc.get();
+        if (!messageRef.exists) {
+            return res.status(400).json({ error: "Message not found" });
+        }
+
+        // Only person, which send message, can delete it
+        if (senderId !== messageRef.data().senderId) {
+            return res.status(403).json({ error: "You can't delete this message" });
+        }
+
+        const conversationSnapshot = await db.collection("conversations")
+            .where("participants", 'array-contains', senderId)
+            .get();
+        
+        const conversationDoc = conversationSnapshot.docs.find(doc => {
+            const participants = doc.data().participants;
+            return participants.includes(messageRef.data().receiverId)
+        });
+
+        if (!conversationDoc) {
+            return res.status(400).json({ error: "Conversation not found" });
+        }
+
+        // Delete message from conversation and directly 
+        await conversationDoc.ref.update({
+            messages: FieldValue.arrayRemove(messageId)
+        });
+        await messageDoc.delete();
+        
+        res.status(200).json({ message: "Message deleted successfully" });
+    
+    } catch (error) {
+        console.log("Error in 'deleteMessage' controller", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+module.exports = { getMessages, sendMessage, deleteMessage };
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { protectRoute } = require('../middleware/protectRoute');
-const { getMessages, sendMessage } = require('../controllers/message.controller');
+const { getMessages, sendMessage, deleteMessage } = require('../controllers/message.controller');
 
 const router = express.Router();
 
 router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
+router.delete("/:id", protectRoute, deleteMessage);
 
 module.exports = router;
