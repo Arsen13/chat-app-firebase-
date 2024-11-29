90a035fcb39055aea7e21df82c784aeded726318
Kohut Arsen <kohut.arsen@gmail.com>
2024-11-29 11:49:39 +0200

    Create 'getUserForSidebar' controller and 'protectRoute' middleware

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,23 @@
-const getUserForSidebar = async (req, res) => {
+const { db } = require("../db/firebase");
 
+const getUserForSidebar = async (req, res) => {
+    try {
+        const currentUser = req.user;
+        const users = await db.collection("users").where("username", '!=', currentUser.username).get();
+        
+        const userList = []
+    
+        users.forEach(user => {
+            userData = user.data();
+            delete userData.password;
+            userList.push(userData);
+        })
+    
+        res.status(200).json(userList)
+    } catch (error) {
+        console.log("Error in 'getUserForSidebar' controller", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
 
-module.exports = { getUserForSidebar };
+module.exports = getUserForSidebar;
--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,5 +1,30 @@
-const protectRoute = async (req, res, next) => {
+const jwt = require("jsonwebtoken");
+const { db } = require("../db/firebase");
 
+const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+        if (!token) {
+            return res.status(401).json({ error: "Unauthorized - No Token Provided" });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if (!decoded) {
+            return res.status(401).json({ error: "Unauthorized - Invalid Token" });
+        }
+
+        const user = await db.collection("users").doc(decoded.userId).get();
+        if (user.empty) {
+            return res.status(400).json({ error: "User not found" });
+        }
+
+        req.user = user.data();
+        next();
+        
+    } catch (error) {
+        console.log("Error in 'protectRoute' middleware", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
 
 module.exports = { protectRoute };
