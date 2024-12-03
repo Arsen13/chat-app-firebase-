a82f8c28dd011b6672fa2a06496676dde8cba9b9
Kohut Arsen <kohut.arsen@gmail.com>
2024-12-03 20:23:17 +0200

    Create frontend part of app. Also create 'Login' and 'SignUp' pages

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Login from "./pages/Login/Login"
+import SignUp from "./pages/SignUp/SignUp"
+
+function App() {
+
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <SignUp />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/SignUp/GenderCheckBox.tsx
@@ -0,0 +1,26 @@
+function GenderCheckBox() {
+  return (
+      <div className="flex">
+        <div className="form-control">
+          <label className="label gap-2 cursor-pointer">
+						<span className="label-text">Male</span>
+						<input 
+							type="checkbox"
+							className="checkbox border-purple-900"
+						/>
+					</label>
+        </div>
+				<div className="form-control">
+					<label className="label gap-2 cursor-pointer">
+							<span className="label-text">Female</span>
+							<input 
+								type="checkbox"
+								className="checkbox border-purple-900"
+							/>
+						</label>
+				</div>
+      </div>
+  )
+}
+
+export default GenderCheckBox;
