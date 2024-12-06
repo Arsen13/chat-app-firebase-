794801ac38c43e885e74ebf36b1c9fb82c9bd9ee
Kohut Arsen <kohut.arsen@gmail.com>
2024-12-06 15:15:30 +0200

    Create 'useSendMessage' hook

--- frontend/src/components/MessageContainer/MessageInput.tsx
+++ frontend/src/components/MessageContainer/MessageInput.tsx
@@ -1,17 +1,48 @@
 import { BsSend } from "react-icons/bs";
 import { HiOutlinePaperClip } from "react-icons/hi2";
+import useSendMessage from "../../hooks/useSendMessage";
+import { ChangeEvent, useState } from "react";
 
 function MessageInput() {
+
+    const [messageText, setMessageText] = useState("");
+    const [file, setFile] = useState<File | null>(null);
+
+    const { loading, sendMessage } = useSendMessage();
+
+    const handleChangeFile = (e: ChangeEvent<HTMLInputElement>) => {
+        const files = e.currentTarget.files;
+        if (files) setFile(files[0]);
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData();
+        
+        if (messageText !== "") formData.append("message", messageText);
+        if (file) formData.append("filename", file);
+        
+        await sendMessage(formData);
+
+        setMessageText("");
+        setFile(null);
+    }
+
     return (
-        <form className="px-4 my-3">
+        <form className="px-4 my-3" onSubmit={handleSubmit}>
             <div className="w-full relative">
                 <input 
                     type="text"
                     placeholder="Send a message"
                     className="border text-sm rounded-lg block w-full p-2.5 bg-gray-900 border-gray-600 text-white"
+                    value={messageText}
+                    onChange={(e) => setMessageText(e.target.value)}
                 />
 
                 <div className="flex flex-row absolute inset-y-0 end-0 items-center pe-3 gap-2">
+                    {file && (
+                        <div>{(file.name).replace(/^(.{0,7}).*?(\.[^.]+)$/, "$1$2")}</div>
+                    )}
                     <div>
                         <label htmlFor="inputFile">
                             <HiOutlinePaperClip className="w-6 h-6 cursor-pointer" />
@@ -21,11 +52,12 @@ function MessageInput() {
                             name="inputFile"
                             id="inputFile"
                             className="hidden"
+                            onChange={handleChangeFile}
                         />
                     </div>
 
                     <button type="submit">
-                        <BsSend className="w-6 h-6" />
+                        {loading ? <span className="loading loading-spinner"></span> : <BsSend className="w-6 h-6" />}
                     </button>
                 </div>
             </div>
---
+++ frontend/src/hooks/useSendMessage.ts
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import useConversation from "../store/useConversation";
+import axios from "axios";
+import toast from "react-hot-toast";
+import axiosInstance from "../utils/axiosInstance";
+
+function useSendMessage() {
+    const [loading, setLoading] = useState(false);
+    const { selectedConversation, messages, setMessages } = useConversation();
+
+    const sendMessage = async (data: FormData) => {
+        setLoading(true);
+        try {
+            const res = await axiosInstance.post(`/api/message/send/${selectedConversation?.id}`, data, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                }
+            });
+
+            setMessages([...messages, res.data]);
+            
+        } catch (error) {
+            if (axios.isAxiosError(error) && error.response) {
+                toast.error(error.response.data.error);
+            } else {
+                console.log(error);
+            }
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return { loading, sendMessage };
+}
+
+export default useSendMessage;
