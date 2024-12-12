249769ca8d80e5c0dcc6f33bb9740849fa9501c1
Kohut Arsen <kohut.arsen@gmail.com>
2024-12-12 19:14:11 +0200

    Add functionality to upload multiple files

--- frontend/src/components/MessageContainer/MessageInput.tsx
+++ frontend/src/components/MessageContainer/MessageInput.tsx
@@ -1,22 +1,17 @@
 import { BsSend } from "react-icons/bs";
 import { HiOutlinePaperClip } from "react-icons/hi2";
 import useSendMessage from "../../hooks/useSendMessage";
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 import { useMessageContext } from "../../context/MessageContext";
 import useUpdateMessage from "../../hooks/useUpdateMessage";
 
 function MessageInput() {
 
-    const [file, setFile] = useState<File | null>(null);
+    const [files, setFiles] = useState<FileList | null>(null);
     const { messageText, setMessageText, updateMessage, setUpdateMessage, messageId } = useMessageContext();
 
     const { loading, sendMessage } = useSendMessage();
     const { updateMessageFunc } = useUpdateMessage();
-
-    const handleChangeFile = (e: ChangeEvent<HTMLInputElement>) => {
-        const files = e.currentTarget.files;
-        if (files) setFile(files[0]);
-    }
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -24,7 +19,11 @@ function MessageInput() {
         const formData = new FormData();
         
         if (messageText !== "") formData.append("message", messageText);
-        if (file) formData.append("filename", file);
+        if (files) {
+            for (let i = 0; i < files.length; i++) {
+                formData.append("filename", files[i]);
+            }
+        } 
         
         if (updateMessage) {
             setUpdateMessage(false);
@@ -34,7 +33,7 @@ function MessageInput() {
         }
 
         setMessageText("");
-        setFile(null);
+        setFiles(null);
     }
 
     return (
@@ -49,9 +48,14 @@ function MessageInput() {
                 />
 
                 <div className="flex flex-row absolute inset-y-0 end-0 items-center pe-3 gap-2">
-                    {file && (
-                        <div>{(file.name).replace(/^(.{0,7}).*?(\.[^.]+)$/, "$1$2")}</div>
-                    )}
+                    {files && files.length > 1 
+                        ? (
+                            <div>{(files[0].name).replace(/^(.{0,7}).*?(\.[^.]+)$/, "$1$2")}</div>
+                        )
+                        : (
+                            <div>{files?.length} files</div>
+                        )
+                    }
                     <div>
                         <label htmlFor="inputFile">
                             <HiOutlinePaperClip className="w-6 h-6 cursor-pointer" />
@@ -61,7 +65,7 @@ function MessageInput() {
                             name="inputFile"
                             id="inputFile"
                             className="hidden"
-                            onChange={handleChangeFile}
+                            onChange={(e) => setFiles(e.target.files)}
                         />
                     </div>
 
